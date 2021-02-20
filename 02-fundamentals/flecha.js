d83bdd933cdca2a0747e007022cc9b052bb84268
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-02-20 14:23:52 -0500

    arrow function node js

---
+++ 02-fundamentals/flecha.js
@@ -0,0 +1,16 @@
+// function sumar ( a, b){
+//     return a+b;
+// };
+
+const sumar = (a,b) =>{
+    return a + b;
+}
+// sumar = sumar2
+const sumar2 = (x,y) => x + y;
+
+console.log(sumar(2,3));
+console.log(sumar2(3,3));
+
+const hola = () => 'Hola mundo';
+console.log(hola());
+
